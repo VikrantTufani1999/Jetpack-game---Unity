7e73fe5162a4b8247cfd2bf287274856ea47f548
Vikrant Tufani <8052007294vicky@gmail.com>
2020-06-06 18:20:38 +0530

    First commit

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public GameObject PrevLHS;
+    public GameObject PrevRHS;
+    public GameObject LHS;
+    public GameObject RHS;
+
+    public GameObject player;
+
+        // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.y > RHS.transform.position.y)
+        {
+            var templeft = PrevLHS;
+            var tempright = PrevRHS;
+            PrevLHS = LHS;
+            PrevRHS = RHS;
+            templeft.transform.position += new Vector3(0 ,10, 0);
+            tempright.transform.position += new Vector3(0, 10, 0);
+            LHS = templeft;
+            RHS = tempright;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody body;
+
+    
+
+    private float Horizontal;
+
+    public float speed = 50f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            body.AddForce(new Vector3(0, speed, 0), ForceMode.Acceleration);
+            
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            body.velocity *= 0.5f;
+            
+            
+        }
+        /*if (Input.GetAxis("Mouse X") > 0)
+        {
+            //Code for action on mouse moving left
+            body.AddForce(new Vector3(5, 0, 0), ForceMode.Force);
+            print("Mouse moved left");
+        }
+        if (Input.GetAxis("Mouse X") < 0)
+        {
+            //Code for action on mouse moving right
+            body.AddForce(new Vector3(-5, 0, 0), ForceMode.Force);
+            print("Mouse moved right");
+        }
+        */
+
+
+    }
+    public void Right()
+    {
+        body.AddForce(new Vector3(30, 0, 0), ForceMode.Acceleration);   
+    }
+
+    public void Left()
+    {
+        body.AddForce(new Vector3(-30, 0, 0), ForceMode.Acceleration);
+    }
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
+     {
+         if(collision.CompareTag("Death"))
+         {
+             Debug.Log("Done");
+            // panel.SetActive(true);
+         }
+     }
+}
